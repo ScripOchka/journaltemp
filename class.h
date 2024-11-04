8273a53210c89f87cc7d3c8ca099777e4cedf1c9
ScripOchka <matvey5689@gmail.com>
2024-11-04 21:13:53 +0300

    fix

--- class.h
+++ class.h
@@ -5,6 +5,8 @@
 
 class Weather
 {
+    public:
+        class TemperatureProxy;
     protected:
         note *magazine;
         int size;
@@ -20,11 +22,27 @@ class Weather
         bool operator!=(const Weather &b);
         Weather operator+(const Weather& b);
         Weather& operator += (const Weather& b);
-	double &operator()(int year1, int month1, int day1);
+        TemperatureProxy operator()(int year1, int month1, int day1);
         void add(int year1, int month1, int day1, double temp1);
         void rm(int year, int month, int day, double temp);
         void print_magazine(int year1, int month1, int day1, int year2, int month2, int day2) const;
         friend std::ostream& operator<<(std::ostream& out, const Weather& number);
+
+    class TemperatureProxy {
+    public:
+        TemperatureProxy(Weather& weather, int year, int month, int day): weather(weather), year(year), month(month), day(day) {}
+
+        TemperatureProxy& operator=(double value) {
+            if (value > -273.15) {
+                weather.add(year, month, day, value);
+            }
+            return *this;
+        }
+
+    private:
+        Weather& weather;
+        int year, month, day;
+    };  
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 int main()
 {
     Weather jour1;
-    jour1(2024, 4, 24) = 15;
+    jour1(2024, 10, 24) = 15;
     jour1(2024, 6, 13) = 10;
-    jour1(2024, 11, 8) = -4;
-    jour1.add(2024, 12, 3, -303);
-    jour1.rm(2024, 11, 8, -4);
+    jour1(2025, 5, 8) = -4;
+    jour1(2024, 12, 3) = -303;
+    //jour1.rm(2024, 11, 8, -4);
     
     Weather jour2 = jour1;
-    //jour1.print_magazine(2023, 12, 12, 2024, 12, 27);
+    jour1.print_magazine(2024, 10, 22, 2024, 12, 27);
     
     std::cout << jour2 << std::endl;
 
@@ -24,9 +24,9 @@ int main()
     jour3.add(2024, 6, 13, 10);
     jour3.add(2024, 12, 8, -4);
     
-    Weather jour4 = jour3 + jour1;
+    //Weather jour4 = jour3 + jour1;
     
-    std::cout << jour4 << std::endl;
+    //std::cout << jour4 << std::endl;
     
     
     
